a48f7ba80da2be462002f55712e6ca1a851562ce
Emp-Norton <jamesjelenko@gmail.com>
2017-04-01 15:41:05 -0700

    Update viginere.rb
    Insignificant: removed ancient notes to self.
    
    Significant: added #downcase to inputs for encoding/decoding. Since this relies on the #ord method, caps have a different value from lowercase letters, which means "ABC" != "abc". Problematic for a en/decode script.

--- viginere.rb
+++ viginere.rb
@@ -2,19 +2,19 @@
 def keychange_to_offset(keyword)
   key_array=keyword.split("") 
   key_array.each_with_index {|v,i| 
-    key_array[i] = ((v.ord-"a".ord)+1).to_i} #+1 necessary otherwise "a" doesn't offset
+    key_array[i] = ((v.ord-"a".ord)+1).to_i} 
   return key_array
 end 
 
 def cipher(string, key)
-  key_array=keychange_to_offset("#{key}") #nice. do this more often (modular code)
+  key_array=keychange_to_offset("#{key}") 
   result="" 
   words=string.split(" ")
   words.each {|word|
     current_word=""
     letters=word.split("") 
     letters.each_with_index{|v,i|
-      v=(((v.ord+key_array[i%key.length])-"a".ord)%26) #spend time thinking about why edge-cases/bookends
+      v=(((v.ord+key_array[i%key.length])-"a".ord)%26) 
       current_word+=(v+"a".ord).chr}
       result+=current_word+" "}
     puts result
@@ -34,18 +34,18 @@ def clear(string, key)
     puts clear
 end 
 
-def blap(type)
+def main(type)
 if type=="encoding"
   puts "What would you like to cipher today?"
-    response=gets.chomp
+    response=gets.chomp.downcase
   puts "What keyword would you like to use?"
-    getkey=gets.chomp
+    getkey=gets.chomp.downcase
   cipher("#{response}" , "#{getkey}")
 elsif type=="decoding"
   puts "Please input the coded text:"
-  coded=gets.chomp
+  coded=gets.chomp.downcase
   puts "Please provide the keyword:"
-  key=gets.chomp
+  key=gets.chomp.downcase
   puts "The text decodes to:" 
   clear("#{coded}" , "#{key}")
 end 
@@ -54,11 +54,9 @@ end
 puts "Welcome to the Viginere Cipher. Are you encoding or decoding today?"
 type=gets.chomp
 if (type=="encoding"||type=="decoding") 
-  blap(type)
+  main(type)
 else 
   puts "Response not valid, please try again."
 end 
 
-
-
-
+ 
